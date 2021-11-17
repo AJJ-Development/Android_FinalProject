62ef94d8ab793dc9ba6f2b0c5cc76588622142bf
jro0100 <jro0100@gmail.com>
2021-11-17 08:45:23 -0500

    Testing if pushing works correctly

--- app/src/main/java/com/example/gamehub/MainActivity.java
+++ app/src/main/java/com/example/gamehub/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "Log out user");
+                Toast.makeText(MainActivity.this, "Logging Out", Toast.LENGTH_SHORT).show();
 
                 // TODO: Add code from parse to log user out
                 goLoginActivity();
