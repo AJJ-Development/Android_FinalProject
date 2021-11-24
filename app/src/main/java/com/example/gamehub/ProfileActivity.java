d56654562e7aaa3787af97a45bbb248d7e8bf5da
jro0100 <jro0100@gmail.com>
2021-11-24 08:13:43 -0500

    Added navigation bar

--- app/src/main/java/com/example/gamehub/ProfileActivity.java
+++ app/src/main/java/com/example/gamehub/ProfileActivity.java
@@ -5,56 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
-    private Button btnLogout;
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
         // Initialize layout objects -------------------------------
-        btnLogout = findViewById(R.id.btnLogout);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         // Button Click Listeners ----------------------------------
-        //--------------- LOGOUT CLICKED ----------------- //
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Log out user");
-                Toast.makeText(MainActivity.this, "Logging Out", Toast.LENGTH_SHORT).show();
-                ParseUser.logOut();
-                goLoginActivity();
-            }
-        });
         //--------------- NAV BUTTON CLICKED ----------------- //
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ProfileActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        goMainActivity();
                         break;
                     case R.id.action_search:
-                        Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ProfileActivity.this, "Search!", Toast.LENGTH_SHORT).show();
                         goSearchActivity();
                         break;
                     case R.id.action_profile:
-                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
-                        goProfileActivity();
+                        Toast.makeText(ProfileActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
                         break;
                     default:
                         break;
@@ -64,21 +47,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    private void goLoginActivity() {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-        finish();
-    }
-
     private void goSearchActivity() {
         Intent i = new Intent(this, SearchActivity.class);
         startActivity(i);
         finish();
     }
 
-    private void goProfileActivity() {
-        Intent i = new Intent(this, ProfileActivity.class);
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }
--- app/src/main/java/com/example/gamehub/SearchActivity.java
+++ app/src/main/java/com/example/gamehub/SearchActivity.java
@@ -5,55 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
-    private Button btnLogout;
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
         // Initialize layout objects -------------------------------
-        btnLogout = findViewById(R.id.btnLogout);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         // Button Click Listeners ----------------------------------
-        //--------------- LOGOUT CLICKED ----------------- //
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Log out user");
-                Toast.makeText(MainActivity.this, "Logging Out", Toast.LENGTH_SHORT).show();
-                ParseUser.logOut();
-                goLoginActivity();
-            }
-        });
         //--------------- NAV BUTTON CLICKED ----------------- //
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SearchActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        goMainActivity();
                         break;
                     case R.id.action_search:
-                        Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_SHORT).show();
-                        goSearchActivity();
+                        Toast.makeText(SearchActivity.this, "Search!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
-                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SearchActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
                         goProfileActivity();
                         break;
                     default:
@@ -64,21 +47,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    private void goLoginActivity() {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-        finish();
-    }
-
-    private void goSearchActivity() {
-        Intent i = new Intent(this, SearchActivity.class);
-        startActivity(i);
-        finish();
-    }
-
     private void goProfileActivity() {
         Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }
