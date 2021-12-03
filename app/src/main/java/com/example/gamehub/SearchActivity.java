be799a9f0b21e68c02f54b97e26e8bdbe6536e05
Atharv-Tyagi <doityagi@gmail.com>
2021-12-03 12:48:39 -0500

    Implemented search feature.

--- app/src/main/java/com/example/gamehub/SearchActivity.java
+++ app/src/main/java/com/example/gamehub/SearchActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
@@ -23,18 +26,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import okhttp3.Headers;
 
 public class SearchActivity extends AppCompatActivity {
 
     public static final String TAG = "SearchActivity";
-    public static final String NOW_PLAYING_URL = "https://api.rawg.io/api/games?key=bb55f483d7464f99917c8e1f821f9cfc";
+    public static final String NOW_PLAYING_URL = "https://api.rawg.io/api/games?key=bb55f483d7464f99917c8e1f821f9cfc&page_size=50";
     private BottomNavigationView bottomNavigationView;
     private RecyclerView rvGames;
+    private EditText etSearchGame;
+    private TextWatcher textWatcher;
     List<Game> games;
+    List<Game> gamesCopy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +50,12 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         rvGames = findViewById(R.id.rvGames_Search);
+        etSearchGame = findViewById(R.id.etSearchGame);
+
+        etSearchGame.requestFocus();
 
         games = new ArrayList<>();
+        gamesCopy = new ArrayList<>();
 
         //Create the adapter
         GameAdapter gameAdapter = new GameAdapter(this, games);
@@ -53,7 +65,6 @@ public class SearchActivity extends AppCompatActivity {
 
         //Set a Layout Manager on the recycler view
         rvGames.setLayoutManager(new LinearLayoutManager(this));
-        //queryGames(gameAdapter);
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
@@ -64,10 +75,8 @@ public class SearchActivity extends AppCompatActivity {
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
-                    Log.i(TAG, "Results: " + results.toString());
-                    games.addAll(Game.fromJsonArray(results));
-                    gameAdapter.notifyDataSetChanged();
-                    Log.i(TAG, "Games: " + games.size());
+                    gamesCopy.addAll(Game.fromJsonArray(results));
+                    //gameAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     Log.e(TAG, "hit json exception", e);
                     e.printStackTrace();
@@ -79,6 +88,36 @@ public class SearchActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure");
             }
         });
+
+        textWatcher = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().equals("")) { gameAdapter.clear(); return; }
+                gameAdapter.clear();
+
+                for (int i = 0; i < gamesCopy.size(); i++) {
+                    if (gamesCopy.get(i).getTitle().toLowerCase().startsWith(editable.toString().toLowerCase())) {
+                        if (!games.contains(gamesCopy.get(i))) {
+                            games.add(gamesCopy.get(i));
+                        }
+                    }
+                }
+                gameAdapter.notifyDataSetChanged();
+            }
+        };
+
+        etSearchGame.addTextChangedListener(textWatcher);
 
         // Initialize layout objects -------------------------------
         bottomNavigationView = findViewById(R.id.bottomNavigation);
